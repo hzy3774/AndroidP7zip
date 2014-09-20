19eac2bef1d8bc049d61d38bc1eafa2b647de823
hogan <hzy3774@qq.com>
2014-09-20 20:52:26 +0800

    change some layout

--- MyZipTest/src/com/example/myziptest/CommandActivity.java
+++ MyZipTest/src/com/example/myziptest/CommandActivity.java
@@ -2,8 +2,14 @@ package com.example.myziptest;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class CommandActivity extends Activity {
+	
+	EditText etCommand = null;
+	Button btExecute = null;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
