3d289ff09e19a63738c508cb2cc274ca53e8e7f7
hogan-office <hzy3774@qq.com>
2014-10-16 17:12:32 +0800

    finish file extract part

--- MyZipTest/src/com/example/myziptest/DecompressActivity.java
+++ MyZipTest/src/com/example/myziptest/DecompressActivity.java
@@ -11,6 +11,7 @@ import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.CompoundButton.OnCheckedChangeListener;
 import android.widget.EditText;
+import android.widget.Spinner;
 
 import com.example.myziptest.R.id;
 
@@ -18,7 +19,8 @@ public class DecompressActivity extends Activity {
 
 	Button btSrc, btDst, btExe;
 	EditText etSrc, etDst, etWildcard, etPassword;
-	CheckBox cbWildcard, cbPassword, cbShowPassword;
+	CheckBox cbWildcard, cbPassword, cbShowPassword, cbFilesOnly;
+	Spinner spOverwriteMode;
 	OnCheckedChangeListener onCheckboxListener = null;
 	OnClickListener onButtonClickListener = null;
 	
@@ -47,7 +49,9 @@ public class DecompressActivity extends Activity {
 		cbWildcard = (CheckBox) findViewById(id.checkBoxDeWildcard);
 		cbPassword = (CheckBox) findViewById(id.checkBoxDePasswd);
 		cbShowPassword = (CheckBox) findViewById(id.checkBoxDePwdVisible);
-		
+		cbFilesOnly = (CheckBox) findViewById(id.checkBoxDeFilesOnly);
+		//Spiner
+		spOverwriteMode = (Spinner) findViewById(id.spinnerDeOverwrite);
 		//button listener
 		onButtonClickListener = new OnClickListener() {
 			@Override
@@ -61,16 +65,7 @@ public class DecompressActivity extends Activity {
 					startFileChooser(FileChooseActivity.FILTER_DIR, REQUEST_CODE_DST);
 					break;
 				case id.buttonDeExecute:
-					StringBuilder sbCmd = new StringBuilder("7z x ");
-					sbCmd.append("'" + etSrc.getText().toString() + "' ");	//7z x 'aaa/bbb.zip'
-					sbCmd.append("'-o" + etDst.getText().toString() + "' ");	//7z x 'a.zip' '-o/out/'
-					if(isWildcard){
-						sbCmd.append("'" + etWildcard.getText().toString() + "' ");	//7z x 'a.zip' '-o/out/' '*.txt'
-					}
-					if(isPassword){
-						sbCmd.append("'-p" + etPassword.getText().toString() + "' ");	//7z x 'a.zip' '-o/out/' '*.txt' -ppwd
-					}
-					new ZipProcess(DecompressActivity.this, sbCmd.toString()).start();
+					extractProcess();
 					break;
 				default:
 					break;
@@ -116,6 +111,40 @@ public class DecompressActivity extends Activity {
 		cbPassword.setOnCheckedChangeListener(onCheckboxListener);
 		cbShowPassword.setOnCheckedChangeListener(onCheckboxListener);
 		
+	}
+	
+	private void extractProcess(){
+		StringBuilder sbCmd = new StringBuilder("7z ");
+		sbCmd.append(cbFilesOnly.isChecked() ? "e " : "x ");	//7z e || 7z x
+		//input file path
+		sbCmd.append("'" + etSrc.getText().toString() + "' ");	//7z x 'aaa/bbb.zip'
+		//output path
+		sbCmd.append("'-o" + etDst.getText().toString() + "' ");	//7z x 'a.zip' '-o/out/'
+		if(isWildcard){
+			sbCmd.append("'" + etWildcard.getText().toString() + "' ");	//7z x 'a.zip' '-o/out/' '*.txt'
+		}
+		if(isPassword){
+			sbCmd.append("'-p" + etPassword.getText().toString() + "' ");	//7z x 'a.zip' '-o/out/' '*.txt' -ppwd
+		}
+		//overwrite mode
+		switch (spOverwriteMode.getSelectedItemPosition()) {
+		case 0:
+			sbCmd.append("-aoa ");	//-aoa Overwrite All existing files without prompt. 
+			break;
+		case 1:
+			sbCmd.append("-aos ");	//-aos Skip extracting of existing files. 
+			break;
+		case 2:
+			sbCmd.append("-aou ");	//-aou aUto rename extracting file (for example, name.txt will be renamed to name_1.txt). 
+			break;
+		case 3:
+			sbCmd.append("-aot ");	//-aot auto rename existing file (for example, name.txt will be renamed to name_1.txt). 
+			break;
+		default:
+			sbCmd.append("-y");	//-y
+			break;
+		}
+		new ZipProcess(DecompressActivity.this, sbCmd.toString()).start();
 	}
 	
 	//open a file choose activity to choose file
