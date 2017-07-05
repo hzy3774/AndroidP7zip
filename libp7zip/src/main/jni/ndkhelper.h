62c6d2cd7ec635d7cf46316ed21ae8f1516e256a
huzongyao <hzy3774@qq.com>
2017-07-05 12:09:57 +0800

    add native code

---
+++ libp7zip/src/main/jni/ndkhelper.h
@@ -0,0 +1,23 @@
+#ifndef ANDROIDUN7ZIP_NDKHELPER_H
+#define ANDROIDUN7ZIP_NDKHELPER_H
+
+#include <jni.h>
+
+#ifdef NDK_DEBUG
+#define LOG_TAG "jniLog"
+#include <android/log.h>
+
+#define LOGD(...) __android_log_print(ANDROID_LOG_DEBUG,LOG_TAG,__VA_ARGS__)
+#define LOGI(...) __android_log_print(ANDROID_LOG_INFO,LOG_TAG,__VA_ARGS__)
+#define LOGW(...) __android_log_print(ANDROID_LOG_WARN,LOG_TAG,__VA_ARGS__)
+#define LOGE(...) __android_log_print(ANDROID_LOG_ERROR,LOG_TAG,__VA_ARGS__)
+#define LOGF(...) __android_log_print(ANDROID_LOG_FATAL,LOG_TAG,__VA_ARGS__)
+#else
+#define LOGD(...) do{}while(0)
+#define LOGI(...) do{}while(0)
+#define LOGW(...) do{}while(0)
+#define LOGE(...) do{}while(0)
+#define LOGF(...) do{}while(0)
+#endif
+
+#endif
